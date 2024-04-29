50b12fb5ecd1ad02fec08c36c0e9739bcff7da98
Yuri Konev <crazy_clown93@mail.ru>
2024-04-29 11:29:59 +0300

    add some variables

--- Lesson_1/Task1/Program.cs
+++ Lesson_1/Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
