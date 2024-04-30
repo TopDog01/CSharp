94b8195471ce838ae9b2346aef6ba9c366f59b35
Yuri Konev <crazy_clown93@mail.ru>
2024-04-30 20:00:17 +0300

    add seminar:task1

--- Seminar/Task1/Program.cs
+++ Seminar/Task1/Program.cs
@@ -6,6 +6,20 @@
 // a = 9, b = -3         => да
 // a = -3, b = 9         => нет
 
-string num1str = Console.ReadLine();
-int num = Convert.ToInt32(num1str);
+//string num1str = Console.ReadLine();
+//int num = Convert.ToInt32(num1str);
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2 * num2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
 
