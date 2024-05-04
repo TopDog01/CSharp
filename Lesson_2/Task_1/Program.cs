0cb2cb2a5da673bb8d0218c08dd8d3519c8d8553
Yuri Konev <crazy_clown93@mail.ru>
2024-05-04 20:58:06 +0300

    add Lesson2:Task1

---
+++ Lesson_2/Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+//Console.WriteLine(arr[5]);
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
