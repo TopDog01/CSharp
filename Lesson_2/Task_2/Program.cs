14b932dd9479228440c04f8f039b853118663dc7
Yuri Konev <crazy_clown93@mail.ru>
2024-05-04 21:29:43 +0300

    add Lesson2:Task2

---
+++ Lesson_2/Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    //Console.Write(arr[i]);
+    //Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
