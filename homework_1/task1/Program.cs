0cd3f741911bcb46eb49ec0545bc34569a245c9d
Yuri Konev <crazy_clown93@mail.ru>
2024-05-04 20:37:37 +0300

    add homewok:task1

---
+++ homework_1/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Внутри класса Answer напишите метод CompareNumbers, который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+
+class Answer {
+    public static void CompareNumbers(int number1, int number2) {
+        if (number1 > number2) {
+            System.out.println("Первое число больше второго: " + number1 + " > " + number2);
+        } else if (number1 < number2) {
+            System.out.println("Второе число больше первого: " + number2 + " > " + number1);
+        } else {
+            System.out.println("Числа равны: " + number1 + " = " + number2);
