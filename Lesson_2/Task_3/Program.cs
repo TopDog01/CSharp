4ffb2d99e6ec5b167308f0b22e10cb18b394608e
Yuri Konev <crazy_clown93@mail.ru>
2024-05-04 21:45:47 +0300

    add Lesson2:Task3

---
+++ Lesson_2/Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] array = { 2, 5, 7, 9, 36, 3, 8, 1, 12, 22 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
