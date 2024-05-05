662fa2dbd11c18905fb8e9f2376cfbc3103de041
Yuri Konev <crazy_clown93@mail.ru>
2024-05-05 21:00:24 +0300

    add Lesson2:Task4

---
+++ Lesson_2/Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 5;
+int[] array = { 2, 6, 9, 3, 4 };
+int i = 0;
+int  max = array[0];
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
