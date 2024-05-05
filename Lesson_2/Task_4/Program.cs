69cb33e04dd2eaae089040324e87fce77f24960b
Yuri Konev <crazy_clown93@mail.ru>
2024-05-05 21:15:59 +0300

    add: Lesson2:Task4

--- Lesson_2/Task_4/Program.cs
+++ Lesson_2/Task_4/Program.cs
@@ -12,3 +12,23 @@ while (i < n)
     i = i + 1;
 }
 Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+     if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
