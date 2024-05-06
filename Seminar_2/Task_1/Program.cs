57b22953dc90275df70b216a109e2c26687be9de
Yuri Konev <crazy_clown93@mail.ru>
2024-05-06 20:44:25 +0300

    division by 100

--- Seminar_2/Task_1/Program.cs
+++ Seminar_2/Task_1/Program.cs
@@ -12,7 +12,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 if (num >= 100 && num <= 999)
 {
-    int firstDigit = num / 100; //256 / 10 = 2.56 = 2
+    int firstDigit = num / 100; //256 / 100 = 2.56 = 2
     int thirdDigit = num % 10; // 256 % 10 = 250 + 6 = 6
 
     int result = firstDigit * 10 + thirdDigit;
