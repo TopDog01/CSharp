8c58c64442b2e899ad39378474dd20caad78c168
Yuri Konev <crazy_clown93@mail.ru>
2024-05-07 19:14:11 +0300

    add Seminar2:Task4

---
+++ Seminar_2/Task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая выводит третью слева
+//цифру заданного числа или сообщает, что третьей
+//цифры нет.
+//456 => 6
+//7812 => 1
+//91 => Третьей цифры нет
+
+Console.WriteLine("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 || num <= -100)
+{
+   while (num > 999 || num < -999)
+   {
+    num = num / 10;
+   } 
+   int result = num % 10;
+   Console.WriteLine("Третья цифра слева: " + result);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+
