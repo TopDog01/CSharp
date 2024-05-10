263a302ae435657c2559fadf8d0b466ae9f02037
Yuri Konev <crazy_clown93@mail.ru>
2024-05-10 11:00:52 +0300

    add solution task4

--- Homework_2/Task_4/Program.cs
+++ Homework_2/Task_4/Program.cs
@@ -1,3 +1,15 @@
 ﻿//Напишите программу, которая на вход принимает натуральное число N, 
 //а на выходе показывает его цифры через запятую.
 
+Console.WriteLine("Введите натуральное число N:");
+string input = Console.ReadLine();
+
+if (Int64.TryParse(input, out long number))
+{
+    Console.WriteLine("Цифры числа N через запятую:");
+    Console.WriteLine(String.Join(",", input.ToCharArray()));
+}
+else
+{
+    Console.WriteLine("Введено не натуральное число.");
+}
