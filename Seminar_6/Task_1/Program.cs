b160b27f258731e02e813e97a9fcacbd7a381198
Yuri Konev <crazy_clown93@mail.ru>
2024-05-20 20:40:19 +0300

    add Seminar6:Task1

---
+++ Seminar_6/Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//1.Задайте массив символов (тип char []). 
+//2.Создайте строку из символов этого массива.
+
+//Указание
+//Конструктор строки вида string(char []) не использовать.
+//Пример
+//[‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+string CharArrayToString(char[] arr)
+{
+    string res = string.Empty;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        res += arr[i];
+    }
+    return res;
+}
+
+char[] array = { 'a', 'b', 'c', 'd', 'e', 'f' };
+string result = CharArrayToString(array);
+Console.WriteLine(result);
