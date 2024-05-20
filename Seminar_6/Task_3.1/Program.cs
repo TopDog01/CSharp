442e7322782ad29bcb2c5e0bbcc09c439a924d03
Yuri Konev <crazy_clown93@mail.ru>
2024-05-20 21:40:05 +0300

    addSeminar6:Task3.1

---
+++ Seminar_6/Task_3.1/Program.cs
@@ -0,0 +1,17 @@
+﻿int CountVowels(string str)
+{
+    string vowels = "aeiouy";
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (vowels.Contains(str[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Напишите слово: ");
+string start = Console.ReadLine();
+Console.WriteLine($"В слове {CountVowels(start)} гласных");
