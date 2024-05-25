3e53857223b845aa1215731cf5d12a3d7a75d5ca
Yuri Konev <crazy_clown93@mail.ru>
2024-05-25 18:03:11 +0300

    add IntermediateCertification:Task1

---
+++ Intermediate_Certification/Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Введите значение N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        
+        PrintNumbersInRange(M, N);
+    }
+    
+    static void PrintNumbersInRange(int start, int end)
+    {
+        if (start <= end)
+        {
+            Console.WriteLine(start);
+            PrintNumbersInRange(start + 1, end);
+        }
+    }
+}
