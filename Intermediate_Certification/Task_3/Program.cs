dc2a116cd1c07a8cab40c4c2c6fc0ff12359176d
Yuri Konev <crazy_clown93@mail.ru>
2024-05-28 21:12:22 +0300

    add IntermediateCertification:Task3

---
+++ Intermediate_Certification/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте произвольный массив.
+//Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+        PrintArrayReverse(array, array.Length - 1);
+    }
+
+    static void PrintArrayReverse(int[] array, int index)
+    {
+        if (index < 0)
+        {
+            return;
+        }
+        Console.WriteLine(array[index]);
+        PrintArrayReverse(array, index - 1);
+    }
+}
